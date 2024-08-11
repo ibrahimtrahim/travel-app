a7cdb99373ee9cd6be973fc1fb3a5b2818f32a9d
ibrahimtrahim <trahimbrahim@gmail.com>
2024-08-11 14:35:21 +0000

    Humberger Menu

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,12 +5,13 @@ type ButtonProps = {
     title : string;
     icon? : string;
     variant : string;
-    full? : boolean
-}
+    full? : boolean;
+    onClick?: () => void;
+    }
 
-const Button = ({type, title, icon, variant, full}: ButtonProps) => {
+const Button = ({type, title, icon, variant, full, onClick}: ButtonProps) => {
   return (
-    <button className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>
+    <button className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`} onClick={onClick}>
         {icon && <Image src={icon} alt={title} width={24} height={24} />}
         <label className='bold-16 whitespace-nowrap cursor-pointer'>{title}</label>
     </button>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,30 @@
+"use client"
+
 import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
 import Button from "./Button"
+import { useState } from "react"
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false)
+    function handleShowNav(){
+        setShowMenu(!showMenu)
+    }
+
   return (
-    <nav className="flexBetween max-container padding-container relative z-30 py-5">
-        <Link href="/">
+    <nav className="flexBetween max-container padding-container lg:justify-center lg:space-x-8 w-full fixed top-5 left-0 right-0 z-30">
+        <Link href="/" className="lg:absolute lg:left-8">
             <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
         </Link>
-        <ul className="hidden h-full gap-12 lg:flex">
+        <ul className={`flex flex-col fixed bg-white top-20 w-full left-0 right-0 lg:flex-row gap-6 border-t border-cyan-300 py-8 scale-50 opacity-0 invisible transition-all duration-500 ease-in-out lg:visible lg:opacity-100 lg:scale-100 lg:border-0 lg:gap-12 lg:static lg:w-auto ${showMenu? 'showNav':''}`}>
             {NAV_LINKS.map((link) => (
                 <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
                     {link.label}
                 </Link>
             ))}
         </ul>
-        <div className="hidden lg:flexCenter">
+        <div className="lg:flexCenter hidden lg:flex lg:absolute lg:right-8">
             <Button
                 type="button"
                 title="Login"
@@ -25,11 +33,12 @@ const Navbar = () => {
             />
         </div>
         <Image 
-            src="menu.svg"
+            src={showMenu ? "/close.svg" : "/menu.svg"}
             alt="menu"
             width={32}
             height={32}
             className="inline-block cursor-pointer lg:hidden"
+            onClick={handleShowNav}
         />
     </nav>
   )
