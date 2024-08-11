366e6a3b1417e91dff76721d753964408e8f66b7
ibrahimtrahim <trahimbrahim@gmail.com>
2024-08-11 06:31:03 +0000

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello, world!</h1>
+    <>
+      <Hero />
+      <Camp/>
+      <Guide/>
+      <Features/>
+      <GetApp />
+
+    </>
   );
 }
